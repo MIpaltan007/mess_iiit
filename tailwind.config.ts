2cab35b01af017b4567fab573cf99ced61d67087
Aditya Bikram Jena <adityabikramjena2002@gmail.com>
2025-05-17 14:30:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Input id="primaryColor" type="color" value="#4CAF50" className="h-10">
    
    Relevant files:
    
        - https://6000-idx-studio-1746512787878.cluster-zkm2jrwbnbd4awuedc2alqxrpk.cloudworkstations.dev/_next/static/chunks/_c115928d._.js
        - https://6000-idx-studio-1746512787878.cluster-zkm2jrwbnbd4awuedc2alqxrpk.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-idx-studio-1746512787878.cluster-zkm2jrwbnbd4awuedc2alqxrpk.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Provide this a functionality so that when a color is changed, the theme is also changed at global.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,8 +21,8 @@ export default {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
+  				DEFAULT: 'var(--primary)', // Changed from hsl(var(--primary))
+  				foreground: 'var(--primary-foreground)' // Changed from hsl(var(--primary-foreground))
   			},
   			secondary: {
   				DEFAULT: 'hsl(var(--secondary))',
@@ -42,7 +42,7 @@ export default {
   			},
   			border: 'hsl(var(--border))',
   			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
+  			ring: 'var(--ring)', // Changed from hsl(var(--ring))
   			chart: {
   				'1': 'hsl(var(--chart-1))',
   				'2': 'hsl(var(--chart-2))',
