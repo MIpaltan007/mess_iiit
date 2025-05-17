165778813830a9a5561fea9151a412b597049b39
Aditya Bikram Jena <adityabikramjena2002@gmail.com>
2025-05-17 05:18:42 +0000

    "config changed"

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,13 +5,12 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 // import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Optional
+  apiKey: "AIzaSyB2Kc7uqhj6CMGQnBk7Zx7ZkD5gOs-U1UQ",
+  authDomain: "meal-plan-hub-h4g8m.firebaseapp.com",
+  projectId: "meal-plan-hub-h4g8m",
+  storageBucket: "meal-plan-hub-h4g8m.firebasestorage.app",
+  messagingSenderId: "836129799986",
+  appId: "1:836129799986:web:274f0cf5a246a679c3a12b"
 };
 
 // Initialize Firebase
